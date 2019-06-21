66b595adde0b741ee5d775795ba3f7ef421fc647
Paris2020 <nelly.moseki@amazeelabs.com>
2019-06-21 19:45:05 +0200

    AFCG: Created new index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Afrihost Group Chat">
+  <meta name="author" content="Nelly Moseki, Junior Frontend Developer">
+  <title>Afrihost Group Chat</title>
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="themes/afrihostchatgroup/styles.css">
+</head>
+
+<body class="grid">
+
+
+   <!-- Javascript files -->
+  <script type="text/javascript"></script>
+</body>
+</html>
